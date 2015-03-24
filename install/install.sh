1b38d807eb751fc96159a6fe5dcabdb8e06dca68
Jeff Zignego <jlzignego@gmail.com>
2015-03-24 00:02:02 -0500

    added rhino to install packages

--- install/install.sh
+++ install/install.sh
@@ -5,10 +5,6 @@ echo "$0 is executing"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-PYTHON="python python-pip python-virtualenv"
-PYTHON3="python3 python3-pip"
-sudo apt-get -y install $PYTHON $PYTHON3
-
 GNU="gcc gdb make automake valgrind"
 LLVM="llvm clang"
 CDEV="flawfinder splint" 
@@ -17,8 +13,15 @@ sudo apt-get -y install $GNU $LLVM $CDEV
 SYSTEM="perl openssh-client vim git"
 sudo apt-get -y install $SYSTEM
 
+PYTHON="python python-pip python-virtualenv"
+PYTHON3="python3 python3-pip"
+sudo apt-get -y install $PYTHON $PYTHON3
+
 NET="wget curl wireshark tshark"
 sudo apt-get -y install $NET
+
+WEB="rhino"
+sudo apt-get -y install $WEB
 
 echo "$0 is exiting"
 exit
