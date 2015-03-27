4e70abc0829dc7a76731f52bf66dd2a45bd5c253
Jeff Zignego <jlzignego@gmail.com>
2015-03-27 17:17:21 -0500

    added python dev packages to install list

--- install/install.sh
+++ install/install.sh
@@ -13,8 +13,8 @@ sudo apt-get -y install $GNU $LLVM $CDEV
 SYSTEM="perl openssh-client vim git ssh-askpass ssh-askpass-gnome"
 sudo apt-get -y install $SYSTEM
 
-PYTHON="python python-pip python-virtualenv"
-PYTHON3="python3 python3-pip"
+PYTHON="python python-pip python-virtualenv python-dev"
+PYTHON3="python3 python3-pip python3-dev"
 sudo apt-get -y install $PYTHON $PYTHON3
 
 NET="wget curl wireshark tshark"
