4d6987d133943eff39d998022659cec70f41a933
Jeff Zignego <jlzignego@gmail.com>
2015-06-03 21:29:41 -0500

    setup.sh now sets up adobe source code pro

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,4 +3,15 @@ echo "$0 is executing"
 
 sudo groupadd docker
 
+# install source code
+mkdir /tmp/adobefont
+cd /tmp/adobefont
+wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip
+unzip 1.017R.zip 
+mkdir -p ~/.fonts
+cp source-code-pro-1.017R/OTF/*.otf ~/.fonts/
+fc-cache -f -v
+cd  ~/
+rm -rf /tmp/adobefont
+
 echo "$0 is exiting"
