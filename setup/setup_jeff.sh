59eb1241257e0490be545bbb521f304cab30b8c5
Jeff Zignego <jlzignego@gmail.com>
2015-08-25 22:55:27 -0500

    added vim as editor to git config

--- setup/setup_jeff.sh
+++ setup/setup_jeff.sh
@@ -3,6 +3,7 @@ echo "$0 is executing"
 
 git config --global user.email "jlzignego@gmail.com"
 git config --global user.name "Jeff Zignego"
+git config --global core.editor vim
 
 sudo usermod -aG docker jeff
 
