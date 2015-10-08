7eaa2c02ddb835dbade936a6cc3f367902ab6301
Jeff Zignego <jlzignego@gmail.com>
2015-10-07 19:55:28 -0500

    added umlet and comments to install.sh

--- install/install.sh
+++ install/install.sh
@@ -5,33 +5,44 @@ echo "$0 is executing"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+# packages required to build contiki
 CONTIKI="build-essential binutils-msp430 gcc-msp430 msp430-libc binutils-avr gcc-avr gdb-avr avr-libc avrdude openjdk-7-jdk openjdk-7-jre ant libncurses5-dev doxygen git"
 sudo apt-get -y install $CONTIKI
 
+# c dev tools
 GNU="gcc gdb make automake valgrind"
 LLVM="llvm clang"
 CDEV="flawfinder splint" 
 sudo apt-get -y install $GNU $LLVM $CDEV
 
+# various system tools
 SYSTEM="perl openssh-server openssh-client vim git zsh"
 RANDOM="dconf-cli"
 sudo apt-get -y install $SYSTEM $RANDOM
 
+# all python packages that we need
 PYTHON="python python-pip python-dev python-flake8 python-mccabe python-coverage"
 PYTHON3="python3 python3-pip python3-dev python3-flake8 python-mccabe python3-coverage"
 MORE_PYTHON="python-virtualenv pylint"
 PYTHON_DATABASE="postgresql-server-dev-all libpq-dev"
 sudo apt-get -y install $PYTHON $PYTHON3 $MORE_PYTHON $PYTHON_DATABASE
 
+# network tools
 NET="wget curl tshark wireshark"
 sudo apt-get -y install $NET
 
+# heroku and dependencies
 HEROKU="ruby ruby2.0"
 sudo apt-get -y $HEROKU 
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+# tools for web dev
 WEB="postgresql rhino nodejs npm poedit"
 sudo apt-get -y install $WEB
+
+# software engineering tools
+SE="umlet"
+sudo apt-get -y install $SE
 
 sudo apt-get -y install apparmor # bug fix
 sudo apt-get -y install docker.io
