160aa0224f5a2e67c409c6b4393846020455cc8c
Jeff Zignego <jeffrey.zignego-ext@jci.com>
2017-05-18 18:40:12 -0500

    Add cmake lint and conan to install

--- install/install.sh
+++ install/install.sh
@@ -25,6 +25,8 @@ sudo apt-get -y install umlet doxygen markdown
 
 # stuff I picked up at work
 sudo apt-get -y install graphviz tree ack-grep cmake exuberant-ctags clang-format colordiff
+sudo pip install conan
+sudo pip install polysquare-cmake-linter
 # more stuff I need at work for supporting other teams
 sudo apt-get -y install jsonlint shellcheck
 
