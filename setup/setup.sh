d2c9eaac1a59c006546b25c742e17776b82fcd9d
Jeff Zignego <jlzignego@gmail.com>
2018-04-28 21:14:55 -0500

    setup.sh: make shellcheck happy

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,22 +7,22 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 sudo groupadd docker
 sudo groupadd wireshark
-sudo usermod -aG docker $(whoami)
-sudo usermod -aG wireshark $(whoami)
+sudo usermod -aG docker "$(whoami)"
+sudo usermod -aG wireshark "$(whoami)"
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
 
 # install source code pro
 mkdir /tmp/adobefont
-cd /tmp/adobefont
+cd /tmp/adobefont || exit 1
 wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip
 unzip 1.017R.zip
 mkdir -p ~/.fonts
 cp source-code-pro-1.017R/OTF/*.otf ~/.fonts/
 fc-cache -f -v
 
-cd ~/
+cd ~/ || exit 1
 # grab oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
 # set zsh as the default shell
 echo "Enter $(whoami)'s password for chsh"
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
