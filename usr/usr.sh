14c738922a779dfbff710e92a390e262e26efcb1
Jeff Zignego <jlzignego@gmail.com>
2019-11-13 23:22:16 -0600

    Fix usr.sh stow command

--- usr/usr.sh
+++ usr/usr.sh
@@ -4,4 +4,4 @@ set -e
 this_script_dir="$(dirname "$(readlink -e "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}" )" )"
 
 # symlink user made scripts to /usr/local/bin so they are available on the PATH
-sudo stow --target=/usr/local/bin "${this_script_dir}/bin"
+sudo stow --dir="${this_script_dir}" --target=/usr/local/bin bin
