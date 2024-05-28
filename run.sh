bf397958043a5c7f85e0dd20e55f5aff8cfab7e5
Jeff Zignego <jlzignego@gmail.com>
2024-05-28 13:54:53 -0500

    run.sh: Add windows support

--- run.sh
+++ run.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
-set -e
+set -eu
 
 this_script_dir="$(dirname "$(readlink -e "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}" )" )"
 
-# ask for password upfront
-sudo -v
-
-# keep-alive; update existing sudo time stamp if set, otherwise do nothing
-while true;
-    do sudo -n true;
-    sleep 60;
-    kill -0 "$$" || exit;
-done 2>/dev/null &
-
-UBUNTU=false
+WINDOWS_NT=false
 NATIVE_LINUX=false
 WSL=false
-if grep -q Microsoft /proc/version; then
+if [ $OS -ne "Windows_NT"];
+  WINDOWS_NT=true
+elif grep -q Microsoft /proc/version; then
   WSL=true
 else
   NATIVE_LINUX=true
-  if grep -q Ubuntu /proc/version; then
-    UBUNTU=true
-  fi
 fi
-export UBUNTU
+
+export WINDOWS_NT
 export NATIVE_LINUX
 export WSL
 
-"${this_script_dir}/install/install.sh" || exit 1
+if [ "$NATIVE_LINUX" = "true" ] || [ "$WSL" = "true" ];
+  # ask for password upfront
+  sudo -v
+  # keep-alive; update existing sudo time stamp if set, otherwise do nothing
+  while true;
+    do sudo -n true;
+    sleep 60;
+    kill -0 "$$" || exit;
+  done 2>/dev/null &
+
+  "${this_script_dir}/install/install.sh" || exit 1
+
+elif [ "$WINDOWS_NT" = "true" ];
+  "${this_script_dir}/install/install_win.sh" || exit 1
+
+else
+  echo "Unsupported OS. Exiting."
+  exit 1
+fi
+
+
 
 "${this_script_dir}/setup/setup.sh" || exit 1
 
