eb5b411034e0d38aa2c8453abc508fc88579b36f
Jeff Zignego <jlzignego@gmail.com>
2024-05-28 11:52:08 -0500

    usr/usr.sh: Add windows support
    Add support for windows by moving everything into a if..fi block that
    excludes windows, at least we won't try to symlink broken scripts.

--- usr/usr.sh
+++ usr/usr.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
+set -eu
 
 this_script_dir="$(dirname "$(readlink -e "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}" )" )"
 
-# symlink user made scripts to /usr/local/bin so they are available on the PATH
-sudo stow --dir="${this_script_dir}" --target=/usr/local/bin bin
+if [ $OS != "Windows_NT"]; then
+  # symlink user made scripts to /usr/local/bin so they are available on the PATH
+  sudo stow --dir="${this_script_dir}" --target=/usr/local/bin bin
+fi
